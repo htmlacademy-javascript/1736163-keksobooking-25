fa3467777d63e3dc3713d4e3d5a96ffaf435bedd
AlxAshl <aashcheulovv@gmail.com>
2022-04-01 00:35:50 +0300

    Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -4,7 +4,6 @@ window.onload = function () {
   const typeField = form.querySelector('#type');
   let minPrice = 0;
 
-
   const pristine = new Pristine(form, {
     classTo: 'ad-form__element',
     errorTextParent: 'ad-form__element',
@@ -14,7 +13,6 @@ window.onload = function () {
     errorTextTag: 'span'
   });
 
-
   form.addEventListener('submit', (evt) => {
     const isValid = pristine.validate();
     if (!isValid) {
@@ -22,31 +20,29 @@ window.onload = function () {
     }
   });
 
-  typeField.addEventListener ('change', ()=> {
-    const typeFieldOption = typeField.value;
-    function minPriceThreshold (value) {
-      switch (value) {
-        case 'bungalow':
-          minPrice = 0;
-          break;
-        case 'flat':
-          minPrice = 1000;
-          break;
-        case 'hotel':
-          minPrice = 3000;
-          break;
-        case 'house':
-          minPrice = 5000;
-          break;
-        case 'palace':
-          minPrice = 10000;
-          break;
-      }
-    }
-    minPriceThreshold(typeFieldOption);
-  });
+  typeField.addEventListener('change', minPriceThreshold);
 
-  function validatePrice (value) {
+  function minPriceThreshold(evt) {
+    switch (evt.target.value) {
+      case 'bungalow':
+        minPrice = 0;
+        break;
+      case 'flat':
+        minPrice = 1000;
+        break;
+      case 'hotel':
+        minPrice = 3000;
+        break;
+      case 'house':
+        minPrice = 5000;
+        break;
+      case 'palace':
+        minPrice = 10000;
+        break;
+    }
+  }
+
+  function validatePrice(value) {
     return value >= minPrice;
   }
 
@@ -55,16 +51,15 @@ window.onload = function () {
   const roomNumberField = form.querySelector('[name = "rooms"]');
   const capacityField = form.querySelector('[name = "capacity"]');
   const capacityOption = {
-    '1': ['1'],
-    '2': ['1', '2'],
-    '3': ['1', '2', '3'],
+    '1':['1'],
+    '2':['1', '2'],
+    '3':['1', '2', '3'],
     '100':['0']
-
   };
-  function validateCapacity () {
+
+  function validateCapacity() {
     return capacityOption[roomNumberField.value].includes(capacityField.value);
   }
-
 
   function getRoomNumberFieldErrorMessage(){
     return `Слишком много гостей для ${roomNumberField.value}
@@ -73,6 +68,5 @@ window.onload = function () {
   pristine.addValidator(roomNumberField,validateCapacity, getRoomNumberFieldErrorMessage);
   pristine.addValidator(capacityField,validateCapacity, /*getCapacityErrorMessage*/);
 };
-
 
 export{};
